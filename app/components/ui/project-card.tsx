23b781782a4b5bcc0202375bda617c5b40f54295
bilelBoulhia <billel.boulahia456@gmail.com>
2025-04-13 03:36:36 +0100

    finishing nmobile

--- app/components/ui/project-card.tsx
+++ app/components/ui/project-card.tsx
@@ -104,32 +104,51 @@ interface ProjectCardPhoneProps {
 const ProjectCardPhone: React.FC<ProjectCardPhoneProps> = ({ ProjectCardContent,index }) => {
     return (
         <div
+
             className='flex justify-center   items-center py-10'>
 
-            <motion.div
+            <div
 
                 className="relative backdrop-filter shadow-4xl rounded-xl overflow-hidden">
 
-                <div className="relative z-10 h-full flex justify-center   items-center flex-col-reverse md:flex-row  ">
+                <div className="relative z-10 h-full  flex justify-center   items-center flex-col-reverse md:flex-row  ">
                     <div
-                        className="relative z-10  p-4 flex w-[50%] flex-col  justify-center  ">
 
-                        <div className="flex items-center justify-between mb-2">
+                        className="relative z-10 overflow-hidden  p-4 flex w-[50%]  flex-col  justify-center  ">
+
+                        <motion.div
+
+
+                            className="flex items-center overflow-hidden justify-between mb-2">
                             <h2 className="text-xl font-semibold text-white">{ProjectCardContent.title}</h2>
                             <Link href={ProjectCardContent.githubUrl} target='_blank'>
-                                <FaGithub className="text-gray-400 cursor-pointer hover:text-gray-500"/>
-                            </Link>
-                        </div>
+                                <motion.div
+                                    initial={{y:'100%'}}
+                                    whileInView={{y:0}}
+                                    viewport={{once:true}}
+                                    transition={{duration:0.5,ease:'easeIn' ,delay: index * 0.6 }}
+
+                                    className='overflow-hidden'>
+                                    <FaGithub className="text-gray-400 cursor-pointer hover:text-gray-500"/>
+                                </motion.div>
+                       </Link>
+                        </motion.div>
 
 
-                        <div className='flex justify-start items-center flex-wrap'>
+                        <div className='flex justify-start overflow-hidden items-center flex-wrap'>
                             {ProjectCardContent.technologies.map((techstack, f) => (
-                                <div
-                                    className='flex items-center text-gray-400 px-3 text-xs py-1 border border-gray-500 rounded-full mr-2 mb-2'>
-                                    <div className="rounded-full h-2 w-2 mr-2"
+                                <motion.div
+
+                                    initial={{y:'-100%'}}
+                                    animate={{y:0}}
+                                    transition={{duration:0.5,ease:'easeIn' ,delay: index * 0.1 }}
+                                    className='flex items-center text-gray-400 px-3 text-xs py-1 border border-neutral-800 rounded-full mr-2 mb-2'>
+                                    <div
+
+                                        className="rounded-full h-2 w-2 mr-2"
                                          style={{backgroundColor: ProjectCardContent.techColors[f % ProjectCardContent.techColors.length]}}></div>
                                     {techstack}
-                                </div>
+                                </motion.div>
                             ))}
                         </div>
 
@@ -141,9 +160,15 @@ const ProjectCardPhone: React.FC<ProjectCardPhoneProps> = ({ ProjectCardContent,
                     <div
 
 
-                        className="w-[50%] z-50">
+                        className="w-[50%] overflow-hidden z-50">
                         <Link href={ProjectCardContent.projectUrl} target='_blank'>
-                            <div className="relative h-[65vh] overflow-hidden">
+                            <motion.div
+
+                                initial={{x:'-100%'}}
+                                animate={{x:0}}
+                                transition={{duration:0.5,ease:'easeIn' ,delay: index * 0.1 }}
+
+                                className="relative  h-[65vh] overflow-hidden">
                                 <Image
                                     src={ProjectCardContent.imageUrl}
                                     alt="Project"
@@ -151,11 +176,11 @@ const ProjectCardPhone: React.FC<ProjectCardPhoneProps> = ({ ProjectCardContent,
                                     objectFit="contain "
 
                                 />
-                            </div>
+                            </motion.div>
                         </Link>
                     </div>
                 </div>
-            </motion.div>
+            </div>
 
         </div>
     );
