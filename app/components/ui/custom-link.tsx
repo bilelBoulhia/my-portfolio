5ff62a0c6db406ef7c0d3d2a57b4fec5790c3a27
bilelBoulhia <billel.boulahia456@gmail.com>
2025-04-16 06:53:12 +0100

    adding cv button

---
+++ app/components/ui/custom-link.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+
+import {easeInOut, motion} from "motion/react"
+import {HiDocumentChartBar} from "react-icons/hi2";
+
+
+interface CustomLinkProps {
+    href: string
+
+}
+
+export default function CustomLink({href,
+
+                                      }: CustomLinkProps) {
+
+    const phrase = "Download Resume"
+
+
+
+    return (
+        <motion.div
+
+            initial={{x: '-200%'}}
+            animate={{x: 0}}
+            exit={{opacity:0}}
+            style={{zIndex: -1}}
+            transition={{
+                duration: 0.3,
+                delay:1.5
+            }}
+            className="inline-flex mt-5 items-center space-x-3  bg-neutral-800 rounded-full"
+
+        >
+            <a
+                href={href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="relative flex items-center"
+            >
+                <div className="bg-gradient-to-tr from-indigo-500 to-purple-700 p-2 rounded-full">
+                    <HiDocumentChartBar className="w-4 h-4 text-white"/>
+                </div>
+                <motion.div
+                    initial={{width:0}}
+                    animate={{width:'100%'}}
+                    transition={{duration:0.3,ease:'linear'}}
+                    className="overflow-hidden ml-2"
+                >
+                    <span className="block font-bold mr-2 text-sm text-white whitespace-nowrap">
+                          {phrase}
+                 </span>
+
+                </motion.div>
+            </a>
+        </motion.div>
+    )
+}
--- app/sections/overview-section.tsx
+++ app/sections/overview-section.tsx
@@ -3,14 +3,13 @@ import {easeInOut, motion} from "motion/react";
 import React from "react";
 import {useMediaQuery} from 'usehooks-ts'
 
+import CustomLink from "@/app/components/ui/custom-link";
+
 const OverviewSection = () => {
     const phone = useMediaQuery('(min-width: 768px)')
     return (
+        <div
 
-
-        <motion.div
-            exit={{opacity: 0}}
-            transition={{delay: 1}}
             className='w-full h-full'>
             <div className=' flex p-3 sm:p-0 flex-col md:inline-flex md:flex-row  md:max-h-[22rem]  gap-4  '>
                 <motion.img
@@ -24,7 +23,7 @@ const OverviewSection = () => {
 
 
                     className='w-[15rem] z-2  self-center rounded-2xl h-auto'
-                    src='https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?cs=srgb&dl=pexels-italo-melo-881954-2379004.jpg&fm=jpg'
+                    src='https://i.ibb.co/qYXysw2y/pexels-italo-melo-881954-2379004-1.png'
                     exit={{
                         x: phone ? '-200%' : '0',
                         y: phone ? '0%' : '-200%'
@@ -39,8 +38,8 @@ const OverviewSection = () => {
                     alt={'s'}
                 />
                 <div className='flex flex-col   mt-10 justify-start items-start'>
-                    <div className='inline-flex gap-4 text-4xl sm:text-5xl overflow-hidden font-bold flex-row '>
-                        <div className='overflow-hidden bg-[#0a0a0a]'>
+                    <div className='inline-flex  gap-4 text-4xl sm:text-5xl overflow-hidden  font-bold flex-row '>
+                        <div className='overflow-hidden  bg-[#0a0a0a]'>
                             <motion.h1
 
                                 initial={{x: '200%'}}
@@ -77,6 +76,8 @@ const OverviewSection = () => {
 
 
                     </div>
+
+
                     <motion.div
 
                         exit={{width: 0}}
@@ -86,29 +87,29 @@ const OverviewSection = () => {
 
                         className='flex overflow-hidden flex-col h-full justify-center  max-w-xl'>
                         <motion.p
-
-
-                            initial={{y:250}}
-                            animate={{y:0}}
+                            initial={{y: 250}}
+                            animate={{y: 0}}
                             exit={{width: 0}}
                             transition={{
                                 delay: 0.8, duration: 0.5
                             }}
 
                             className=' text-xl  sm:text-2xl  font-semibold'>hey there👋,
-                            I am a FullStack Software Devoloper,
-                            specialized in web and mobile applications development using .Net and JavaScript
-                            frameworks.
-                            Also a huge warhammer fan ,
-                            I create and contribute to amazing web applications making the internet a better
-                            place.
-                            You can talk to me about Dj
+                            I am a Full Stack Software Developer with a focus on building web and mobile applications
+                            using .NET, Java,
+                            and JavaScript frameworks like React and Next.js. i enjoy programming as much as a tech
+                            priest enjoys his daily prayer to the machine spirit.
+
                         </motion.p>
+                        <div className=' overflow-hidden'>
+                            <CustomLink href={'https://turquoise-cynthie-96.tiiny.site'}/>
+                        </div>
+
                     </motion.div>
                 </div>
             </div>
 
-        </motion.div>
+        </div>
 
 
     )
