e5b81aa6d85d2186575bd1f7d325913b669686a1
bilelBoulhia <billel.boulahia456@gmail.com>
2025-04-20 04:46:41 +0100

    removing unused deps

--- app/sections/contact-section.tsx
+++ app/sections/contact-section.tsx
@@ -78,18 +78,14 @@ export default function ContactSection() {
 
                 className="mt-16 bg-[#111] rounded-xl p-6  border border-[#222]">
                 <div className="inline-flex flex-row items-center justify-between gap-15">
-                    <div className="text-center ">
-                        <h3 className="text-2xl font-bold text-white mb-2">Current Status <span
-                            className="inline-block w-3 h-3 rounded-full bg-green-500 mr-2 animate-pulse"></span></h3>
 
-                    </div>
 
                     <div className="inline-flex justify-evenly items-center  gap-6">
                         <div className="text-center">
                             <div className="text-3xl font-bold text-blue-500 mb-1">
 
                                 12h</div>
-                            <div className="text-sm text-gray-400">Response Time</div>
+                            <div className="text-sm text-gray-400 whitespace-nowrap">Response Time</div>
                         </div>
                         <div className="text-center overflow-hidden">
                             <h3 className="text-3xl z-15  font-bold text-purple-500 overflow-hidden mb-1">
